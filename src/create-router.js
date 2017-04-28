8697cb5881ea0463e62a71512be2a6ff03f78762
Erin Millard <ezzatron@gmail.com>
2017-04-28 17:16:25 +1000

    Removing listeners plugin to demonstrate issue.

--- src/create-router.js
+++ src/create-router.js
@@ -1,5 +1,4 @@
 import browserPlugin from 'router5/plugins/browser'
-import listenersPlugin from 'router5/plugins/listeners'
 import createRouter5 from 'router5'
 import loggerPlugin from 'router5/plugins/logger'
 
@@ -8,7 +7,6 @@ export default function createRouter (options) {
 
   const router = createRouter5([], options)
   router.usePlugin(browserPlugin())
-  router.usePlugin(listenersPlugin())
   if (enableDebugLogs) router.usePlugin(loggerPlugin)
 
   return router
